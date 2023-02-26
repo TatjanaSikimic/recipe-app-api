b5369fd987a75aa6c0847a32d5132a51fe69a5c0
Tatjana <tatjanasikimic16@gmail.com>
2023-02-26 17:05:31 +0100

    Configured docker compose and checks to wait for db.

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -9,6 +9,7 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test commands."""
@@ -19,7 +20,7 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
@@ -31,7 +32,4 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(database=['default'])
-
-
-
+        patched_check.assert_called_with(databases=['default'])
